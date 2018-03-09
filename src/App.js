b5d43f5e2917fcf86663bf38508cbee90651306c
Thien Nguyen <thtnguyen93@gmail.com>
2018-03-09 00:35:26 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import {myAbout,portfolio,resume,testimonials} from './resumeData'
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      
+      resumeData:{
+        myAbout: myAbout,
+        portfolio: portfolio,
+        resume: resume,
+        testimonials: testimonials
+      }
+    }
+  }
+
+
+
+
+  render() {
+    console.log(myAbout);
+    return (
+      <div className="App">
+        <Header data={myAbout} />
+        <About data={this.state.resumeData.myAbout} />
+      </div>
+    );
+  }
+}
+
+export default App;
